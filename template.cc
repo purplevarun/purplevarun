8d3d20ad027dd7e8ab9cb54ba4fe6051d99f3d47
Varun Kedia <39029055+purplevarun@users.noreply.github.com>
2021-07-26 04:48:07 +0530

    Update template.cc

--- template.cc
+++ template.cc
@@ -1,45 +1,56 @@
-// chunchunmaru
 #include <bits/stdc++.h>
-#define int int64_t
-using namespace std;
-#define dbg(x) cout << #x; debug(x);
+#define int long long
 #define all(x) x.begin(), x.end()
-template<typename T>void debug(vector<T> x){
-    cout << " : [ ";
-    for (int i=0;i<x.size()-1;++i)
-        cout << x[i] << ", ";
-    cout << x[x.size()-1] << " ]" << '\n';
+#define vi vector <int>
+#define pb push_back
+using namespace std;
+template<typename T>void debug(vector<T> x) {
+	cout << " : [ ";
+	for (int i=0; i<x.size()-1; ++i)
+		cout << x[i] << ", ";
+	cout << x[x.size()-1] << " ]" << '\n';
 }
-template<typename T, typename S>void debug(unordered_map<T,S> x){
-    cout << " : \n{\n";
-    for (auto i : x){
-        cout << "    " << i.first << " : " << i.second << '\n';
-    }
-    cout << "}" << '\n';
+template<typename T, typename S>void debug(unordered_map<T,S> x) {
+	cout << " : \n{\n";
+	for (auto i : x) {
+		cout << "    " << i.first << " : " << i.second << '\n';
+	}
+	cout << "}" << '\n';
 }
-template<typename T, typename S>void debug(map<T,S> x){
-    cout << " : \n{\n";
-    for (auto i : x){
-        cout << "    " << i.first << " : " << i.second << '\n';
-    }
-    cout << "}" << '\n';
+template<typename T, typename S>void debug(map<T,S> x) {
+	cout << " : \n{\n";
+	for (auto i : x) {
+		cout << "    " << i.first << " : " << i.second << '\n';
+	}
+	cout << "}" << '\n';
 }
-template<typename T>void debug(T x){
-    cout << " : " << x << '\n';
+template<typename T>void debug(T x) {
+	cout << " : " << x << '\n';
 }
-template<typename T>void debug(set<T> x){
-    vector<T> temp;
-    for (auto i:x)temp.push_back(i);
-    debug(temp);
+template<typename T>void debug(set<T> x) {
+	vector<T> temp;
+	for (auto i:x)temp.push_back(i);
+	debug(temp);
 }
-void kotatsu() {
-    
+// ---------------------------------------------------------------------------------------
+void onigiri () {
+	cout << "nanda anta eogo sabetoruu" << endl;
 }
-int32_t main () {
-    ios_base::sync_with_stdio(0);cin.tie(0);
-    int t = 1; 
-    // cin >> t;
-    while (t--){
-        kotatsu();
-    }
+// ---------------------------------------------------------------------------------------
+signed main () {
+#ifndef VarunDebug
+	ios_base::sync_with_stdio(0);
+	cin.tie (0);
+	cout.tie (0);
+#endif
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		onigiri ();
+#ifdef VarunDebug
+		for (int i=0; i<40; ++i)
+			cout << "-";
+		cout << "\n";
+#endif
+	}
 }
